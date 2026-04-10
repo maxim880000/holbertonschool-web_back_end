aed1a99f631169a0bf9d7e9258440e3cf977327f
maxim880000 <mdholberton@gmail.com>
2026-04-10 15:07:24 +0200

    feat(async): collect 10 random numbers with async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,5 +5,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
-    pass
+async def async_comprehension() -> List[float]:
+    """Collect 10 random numbers using an async comprehension over async_generator"""
+    return [number async for number in async_generator()]
