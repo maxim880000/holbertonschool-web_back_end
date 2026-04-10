5a246745ca607088abcbc455a64fe2a7a6668386
maxim880000 <mdholberton@gmail.com>
2026-04-10 15:09:18 +0200

    feat(async): run 4 parallel comprehensions and measure total runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,5 +6,8 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
-    pass
+async def measure_runtime() -> float:
+    """Execute async_comprehension four times in parallel and return total runtime"""
+    start = time.perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    return time.perf_counter() - start
