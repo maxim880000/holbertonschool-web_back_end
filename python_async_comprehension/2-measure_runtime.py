3d643edc402f1ed16fa72c974e6b83ff1797252b
maxim880000 <mdholberton@gmail.com>
2026-04-10 15:08:56 +0200

    feat(async): add imports for asyncio, time and async_comprehension

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 """Module for measuring runtime of parallel async comprehensions"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime():
