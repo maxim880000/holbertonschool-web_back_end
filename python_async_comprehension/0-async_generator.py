2a0ed9812f3d1911537274c029564a3dbf645035
maxim880000 <mdholberton@gmail.com>
2026-04-10 11:06:32 +0200

    chore(async): add return type annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Module for async generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that yields 10 random numbers asynchronously"""
     for _ in range(10):
         await asyncio.sleep(1)
