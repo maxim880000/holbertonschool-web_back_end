e940429cc25f81433848d2d43039e7ee29f42a50
maxim880000 <mdholberton@gmail.com>
2026-05-12 13:56:44 +0200

    adding teh task 0

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,6 @@
+function displayMessage(message) {
+	// Affiche le message reçu en paramètre dans le terminal (STDOUT)
+    console.log(message);
+}
+// Rend la fonction accessible depuis d'autres fichiers avec require()
+module.exports = displayMessage;
