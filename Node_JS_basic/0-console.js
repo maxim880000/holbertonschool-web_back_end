1a508b5bf12a6daee1f9f320f4ee6af87e54b6e9
maxim880000 <mdholberton@gmail.com>
2026-05-12 14:00:59 +0200

    deleted the commetn on task 0

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,5 @@
 function displayMessage(message) {
-    // Affiche le message reçu en paramètre dans le terminal (STDOUT)
-    console.log(message);
+  console.log(message);
 }
-// Rend la fonction accessible depuis d'autres fichiers avec require()
+
 module.exports = displayMessage;
