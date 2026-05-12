b616dd1694e8456aeb7c3e436747144327c282ef
maxim880000 <mdholberton@gmail.com>
2026-05-12 13:58:04 +0200

    correct synthax

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 function displayMessage(message) {
-	// Affiche le message reçu en paramètre dans le terminal (STDOUT)
+    // Affiche le message reçu en paramètre dans le terminal (STDOUT)
     console.log(message);
 }
 // Rend la fonction accessible depuis d'autres fichiers avec require()
